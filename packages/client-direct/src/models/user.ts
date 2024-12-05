7167eae503750d835460f7a25beb2c9d4f81306a
TheCleopatra <cleopatracaesar8@gmail.com>
2024-12-05 22:57:25 +0800

    refactor: make email field optional in user model
    - Commented out the unique constraint on the email field in the user schema to allow for optional email entries.
    - This change enhances flexibility in user registration while maintaining the overall structure of the user model.

--- packages/client-direct/src/models/user.ts
+++ packages/client-direct/src/models/user.ts
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         // required: true,
-        unique: true,
+        // unique: true,
     },
     password: {
         type: String,
